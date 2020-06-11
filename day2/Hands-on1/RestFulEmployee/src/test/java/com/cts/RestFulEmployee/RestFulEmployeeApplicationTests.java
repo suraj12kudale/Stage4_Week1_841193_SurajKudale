47a9abf98622da203078c94e4e48e6f6f1dfc547
suraj12kudale <45892686+suraj12kudale@users.noreply.github.com>
2020-06-11 16:20:19 +0530

    Add files via upload

---
+++ day2/Hands-on1/RestFulEmployee/src/test/java/com/cts/RestFulEmployee/RestFulEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.RestFulEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFulEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
